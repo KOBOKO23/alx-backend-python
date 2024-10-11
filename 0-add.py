4fe54683baf03802d05ecef4da503c53e2c96d4e
KOBOKO23 <kobokomasero@gmail.com>
2024-10-11 15:32:56 +0000

    0-add.py

--- 0-add.py
+++ 0-add.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
-"""Type-annotated function add"""
+"""
+This is a module that provides a function for adding two numbers.
+"""
 
 
 def add(a: float, b: float) -> float:
-    """Takes two floats and returns their sum as float"""
+    """
+    This function adds two numbers and returns the result.
+
+    Parameters:
+    a (float): The first number.
+    b (float): The second number.
+
+    Returns:
+    float: The sum of a and b.
+    """
     return a + b
